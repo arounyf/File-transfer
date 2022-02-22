9c2fa8619c7c1e771d90b875e0146957b09e04f5
liang23 <62204996+liang23cn@users.noreply.github.com>
2022-02-22 21:39:25 +0800

    Add files via upload

--- function/fastd.php
+++ function/fastd.php
@@ -12,31 +12,23 @@ if(!file_exists("../upload/$str")){
     $str = fread($fp,filesize($file_path));
     fclose($fp);
     $startdate = $str;
-    // echo $startdate;
-    
     //计算时间差
     $nowdate = date("Y-m-d H:i:s",time());
     $minute=floor((strtotime($nowdate)-strtotime($startdate))%86400/60);
-    // echo "<br>";
-    // echo $minute;
     //如果时间小于5分钟则不下载
-    if ($minute<5){
+    if ($minute<0){
         echo('<script>alert("歇息会吧，请待会再来");top.location="../index.php";</script>'); 
     }else{
         //高速下载配置
-
-        copy("../upload/$cosfile",$cosdown);
-        
         $downloadurl = get_downurl($cosurl,$cos);
         Header("HTTP/1.1 303 See Other"); 
         header("Location: $downloadurl");
         
         // 发送消息到微信
         $message = "文件下载中  ".$cosfile;
-    	$desp = $message;
-        sct_send($desp);
+        sct_send($message);
         
-        // 文件上传成功后写入历史记录
+        // 写入当前下载文件的时间
         $file_path = "../config/flag";
         $fp = fopen($file_path,"w");
         fwrite($fp,date("Y-m-d H:i",time()));
--- function/upload.php
+++ function/upload.php
@@ -1,19 +1,26 @@
 <?php
 
 require "../config/main.php";
-
 $fname = $_FILES["userfile"]["name"];
 $tsize = round($_FILES["userfile"]["size"] / 1048576,2);
 $temp = explode(".",$fname);
 $extension = end($temp);     // 获取文件扩展名
-
-
 $allow = in_array($extension, $allowedExts);
+
+
+//获取历史文件名
+$file_path = "../config/db";
+$fp = fopen($file_path,"r");
+$str = fread($fp,filesize($file_path));
+fclose($fp);
+$hfname = $str;
+$fnamepath = "../upload/".$str;
+
 
 //多重验证正确性
 if (!$allow){
 	if($tsize < $fsize){
-		upfile($stu_name,$fname,$tsize);
+		upfile($fname,$tsize,$hfname,$fnamepath);
 	}else{
 		echo '<li class="list-group-item">'."文件过大 目前只支持".$fsize.'MB以内的文件</li>';
 	}
@@ -23,8 +30,7 @@ if (!$allow){
 
 
 //文件上传函数
-function upfile($stu_name,$fname,$tsize){
-
+function upfile($fname,$tsize,$hfname,$fnamepath){
 	require "../config/api.php";
  	if ($_FILES['userfile']['error'] > 0) { 
     	echo "错误：: " . $_FILES["userfile"]["error"] . "<br>";
@@ -34,43 +40,30 @@ function upfile($stu_name,$fname,$tsize){
 		echo '<li class="list-group-item">'."文件类型: " . $_FILES["userfile"]["type"] . "<br>"."</li>";
 		echo '<li class="list-group-item">'."文件大小: " . $tsize . " MB<br>"."</li>";
 		echo "</li>";
-		if (file_exists("../upload/" . $fname))
-		{
+		if ($hfname == $fname){
 			echo '<li class="list-group-item">'. " 出现相同文件名，提交失败。 "."</li>";
 		}else{
 			move_uploaded_file($_FILES["userfile"]["tmp_name"], "../upload/" . $fname);
 			echo '<li class="list-group-item">'. "文件提交成功"."</li>";
 			
-            // 发送消息到微信
+		    // 发送消息到微信
             $message = "文件上传成功  ".$fname;
-		    $desp = $message;
-            sct_send($desp);
+            sct_send($message);
             
-             // 读取上次上传的文件名然后删除掉，使得系统中只存在最后一个上传的文件
-            $file_path = "../config/db";
-            $fp = fopen($file_path,"r");
-            $str = fread($fp,filesize($file_path));
-            fclose($fp);
-            $fnamepath="../upload/".$str;
-            $fnamepath2="/www/cosfs/liang23/".$str;
-                        
-            if(!file_exists($fnamepath)){
-            }else{
-                unlink( $fnamepath);
-            }
-            if(!file_exists($fnamepath2)){
-            }else{
-                unlink( $fnamepath2);
-            }
-                
-             // 文件上传成功后写入历史记录
+            // 文件上传成功后写入历史记录
             $file_path = "../config/db";
             $fp = fopen($file_path,"w");
             fwrite($fp,$fname);
             fclose($fp);
-		
+            
+            // 删除缓存文件
+            if(!file_exists($fnamepath)){
+                echo '<li class="list-group-item">'. "历史文件不存在"."</li>";
+            }else{
+                unlink($fnamepath);
+            }
 		}
-	
+		
 	}
 }
 
